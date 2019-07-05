1499994b01e784f203b45293e693d0bce81acd9f
John Bogart <john.bogart@bbc.co.uk>
2019-07-05 12:21:26 +0100

    Shuffle function

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const generateDeck = require('./generateDeck');
 
-console.log(generateDeck());
+const deck = generateDeck();
+
+const shuffle = cards => {
+  for (let i = cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]];
+  }
+  return cards;
+};
+
+console.log(shuffle(deck));
