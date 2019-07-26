bf07f3e9fead26fa55645eaee3485cf96e2ac76e
John Bogart <john.bogart@bbc.co.uk>
2019-07-26 13:51:47 +0100

    use chalk to style command line

--- suits/diamonds.js
+++ suits/diamonds.js
@@ -1,54 +1,56 @@
+const chalk = require('chalk');
+
 const diamonds = [
   {
-    name: '♦A',
+    name: chalk.red('♦A'),
     value: 'ace'
   },
   {
-    name: '♦2',
+    name: chalk.red('♦2'),
     value: 2
   },
   {
-    name: '♦3',
+    name: chalk.red('♦3'),
     value: 3
   },
   {
-    name: '♦4',
+    name: chalk.red('♦4'),
     value: 4
   },
   {
-    name: '♦5',
+    name: chalk.red('♦5'),
     value: 5
   },
   {
-    name: '♦6',
+    name: chalk.red('♦6'),
     value: 6
   },
   {
-    name: '♦7',
+    name: chalk.red('♦7'),
     value: 7
   },
   {
-    name: '♦8',
+    name: chalk.red('♦8'),
     value: 8
   },
   {
-    name: '♦9',
+    name: chalk.red('♦9'),
     value: 9
   },
   {
-    name: '♦10',
+    name: chalk.red('♦10'),
     value: 10
   },
   {
-    name: '♦J',
+    name: chalk.red('♦J'),
     value: 10
   },
   {
-    name: '♦Q',
+    name: chalk.red('♦Q'),
     value: 10
   },
   {
-    name: '♦K',
+    name: chalk.red('♦K'),
     value: 10
   }
 ];
