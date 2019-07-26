297211e817bac6347bdbe9ff69defb8f79e4453d
John Bogart <john.bogart@bbc.co.uk>
2019-07-26 11:15:05 +0100

    Add basic game functionality

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const generateDeck = require('./generateDeck');
 
-const deck = generateDeck();
-
 const shuffle = cards => {
   for (let i = cards.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -10,4 +8,15 @@ const shuffle = cards => {
   return cards;
 };
 
-console.log(shuffle(deck));
+const deal = cards => {
+  return cards.splice(0, 2);
+}
+
+const play = () => {
+  console.log("Welcome to BlackJack!");
+  let deck = generateDeck();
+  deck = deal(shuffle(deck));
+  console.log("Your cards are", deck[0].name, deck[1].name);
+}
+
+console.log(play());
