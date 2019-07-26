b85a4ebbde1cd255fedb06dd56033747b2706b38
John Bogart <john.bogart@bbc.co.uk>
2019-07-26 13:57:53 +0100

    Add more chalk cause it makes it look nice

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+const chalk = require('chalk');
+
 const generateDeck = require('./game/generatedeck');
 const shuffle = require('./game/shuffle');
 const deal = require('./game/deal');
 
 const play = () => {
-  console.log('Welcome to BlackJack!');
+  console.log(chalk.black.bgGreen('Welcome to BlackJack!'));
   let deck = generateDeck();
-  console.log(deck.length);
   deck = deal(shuffle(deck));
-  console.log('Your cards are', deck[0].name, deck[1].name);
-  console.log(deck.length);
-  console.log('Current total: ', deck[0].value + deck[1].value);
+  console.log(chalk.black.bgGreen('Your cards are', deck[0].name, deck[1].name));
+  console.log(chalk.black.bgGreen('Current total: ', deck[0].value + deck[1].value));
 };
 
 console.log(play());
