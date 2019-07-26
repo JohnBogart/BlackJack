b7a353e16a597270a6f602dfeef31be5d0beadec
John Bogart <john.bogart@bbc.co.uk>
2019-07-26 13:22:03 +0100

    Move shuffle function to own module

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const generateDeck = require('./generateDeck');
-
-const shuffle = cards => {
-  for (let i = cards.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [cards[i], cards[j]] = [cards[j], cards[i]];
-  }
-  return cards;
-};
+const shuffle = require('./shuffle');
 
 const deal = cards => {
   return cards.splice(0, 2);
-}
+};
 
 const play = () => {
-  console.log("Welcome to BlackJack!");
+  console.log('Welcome to BlackJack!');
   let deck = generateDeck();
+  console.log(deck.length);
   deck = deal(shuffle(deck));
-  console.log("Your cards are", deck[0].name, deck[1].name);
-}
+  console.log('Your cards are', deck[0].name, deck[1].name);
+  console.log(deck.length);
+  console.log('Current total: ', deck[0].value + deck[1].value);
+};
 
 console.log(play());
